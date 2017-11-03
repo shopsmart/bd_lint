99b8fc342e9f8ad7e6c30de10c405893792fe308
Eric Agnew <wenga.eric@gmail.com>
2017-11-03 14:40:07 -0500

    Add generator for precommit hook

---
+++ lib/generators/pre_commit/pre_commit_generator.rb
@@ -0,0 +1,10 @@
+class PreCommitGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  DESTINATION = ".git/hooks/pre-commit".freeze
+
+  def copy_pre_commit
+    create_file "pre-commit", DESTINATION
+    chmod(DESTINATION, 0755)
+  end
+end
