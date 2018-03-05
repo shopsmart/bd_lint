9acacd6a6f788c789c36937b5805df8460ae3bfb
Eric Agnew <wenga.eric@gmail.com>
2018-03-05 09:33:17 -0600

    Prevents test suite from issues with validator

--- test_app/config/application.rb
+++ test_app/config/application.rb
@@ -6,6 +6,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
+# Prevents test from not running
+require "rake"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
