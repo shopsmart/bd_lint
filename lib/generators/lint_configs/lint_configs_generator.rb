a6af1180d81e6f74a1c37af264b9fa2e959f3e23
Eric Agnew <wenga.eric@gmail.com>
2019-06-20 23:22:30 -0500

    Expose new config to generators

--- lib/generators/lint_configs/lint_configs_generator.rb
+++ lib/generators/lint_configs/lint_configs_generator.rb
@@ -2,6 +2,7 @@ class LintConfigsGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_lint_configs
+    copy_file ".eslintrc.js",            ".eslintrc.js"
     copy_file ".jscsrc",            ".jscsrc"
     copy_file ".pre_commit.ignore", ".pre_commit.ignore"
     copy_file ".rubocop.yml",       ".rubocop.yml"
